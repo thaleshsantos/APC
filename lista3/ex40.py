4e21863cde9c8b37229dcd8b23e1d4ee598cc13b
ths7 <120137721+ths7@users.noreply.github.com>
2023-01-03 09:34:40 -0300

    Update ex40.py

--- lista3/ex40.py
+++ lista3/ex40.py
@@ -6,17 +6,4 @@ for i in range(2,year):
         primo = False
         
 print('Emidio' if primo and year > 1 else 'Faina')
-def chinelo(pe_do_bill, l):
-    
-    l.sort()
-    i = 0
-    while ((i < len(l)) and (l[i] < pe_do_bill)):
-        i += 1
-    if(i == len(l)):
-        return -1
-    else:
-        return i
 
-v = int(input())
-l = [int(i) for i in input().split()]
-print(chinelo(v, l))
